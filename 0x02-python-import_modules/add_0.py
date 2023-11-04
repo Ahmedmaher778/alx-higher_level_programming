180447188c012b1cd0ebecec3126f46c6bd68798
Ahmedmaher778 <ahmedmaher7200@gmail.com>
2023-11-03 17:21:27 -0700

    d

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def add(s, d):
+    """My addition function
+
+    Args:
+        s: first integer
+        d: second integer
+
+    Returns:
+        The return value. s + d
+    """
+    return (s + d)
