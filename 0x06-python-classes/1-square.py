72685848bf5bb81c25b5562cfbde07220aae632c
Ahmedmaher778 <ahmedmaher7200@gmail.com>
2023-11-21 13:32:10 -0800

    f

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(slf, sze):
+        """Initialize a new Square.
+        Args:
+            sze (int): The size of the new square.
+        """
+        slf.__sze = size
