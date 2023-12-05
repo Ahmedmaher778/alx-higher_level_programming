a898b50d87b53538e2e45923a77cfe0d1d9a9aa0
Ahmedmaher778 <ahmedmaher7200@gmail.com>
2023-12-05 15:08:26 -0800

    c

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -4,5 +4,5 @@
 def append_write(filename="", text=""):
     """Appends a string to the end of a UTF8 text file.
     """
-    with open(filename, "b", encoding="utf-8") as my_file_first:
+    with open(filename, "a", encoding="utf-8") as my_file_first:
         return my_file_first.write(text)
