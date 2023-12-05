0cee162e3204c3f061a7d921bf14d86fa8c92969
Ahmedmaher778 <ahmedmaher7200@gmail.com>
2023-12-05 14:41:35 -0800

    c

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -10,5 +10,5 @@ def append_after(filename="", search_string="", new_string=""):
             text += line
             if search_string in line:
                 text += new_string
-    with open(filename, "w") as w:
-        w.write(text)
+    with open(filename, "s") as s:
+        s.write(text)
